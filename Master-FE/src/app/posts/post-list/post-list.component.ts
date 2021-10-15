fb7af825bd9101938b1e87390e7d73cb5d844881
isidora_bukusic <isidora@getadigital.com>
2021-10-15 10:03:51 +0200

    add file saver

--- Master-FE/src/app/posts/post-list/post-list.component.ts
+++ Master-FE/src/app/posts/post-list/post-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { AuthService } from "src/app/auth/auth.service";
 import { Post } from "../post.model";
-import { PostCreateComponent } from "../post-create/post-create.component";
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
@@ -9,22 +8,28 @@ import { PostCreateComponent } from "../post-create/post-create.component";
 })
 export class PostListComponent implements OnInit {
   @Input() post: Post;
+  @Output() onDownload: EventEmitter<void> = new EventEmitter();
   isAuthenticated = false;
   userId: string;
 
-  constructor(public authService: AuthService) {
-  }
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {
     this.isAuthenticated = this.authService.getIsAuth();
     this.userId = this.authService.getUserId();
   }
 
+  onDownloadClick() {
+    this.onDownload.emit();
+  }
+
   onCopy() {
-    var copyText = document.getElementById("copyServicePath") as HTMLInputElement;
+    var copyText = document.getElementById(
+      "copyServicePath"
+    ) as HTMLInputElement;
     copyText.select();
     console.log(copyText.value);
-    
+
     navigator.clipboard.writeText(copyText.value);
   }
 }
